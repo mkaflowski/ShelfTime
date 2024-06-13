d1065ab09faee97b4c74d8bceb6bf8a5cce11cd9
Mateusz Kaflowski <mkaflowski@gmail.com>
2024-06-13 17:49:40 +0200

    demo

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,7 +14,7 @@ android {
         minSdk = 30
         targetSdk = 34
         versionCode = 1
-        versionName = "1.0"
+        versionName = "0.9"
         vectorDrawables {
             useSupportLibrary = true
         }
